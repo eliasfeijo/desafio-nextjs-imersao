b148430bfd67d1cafd940622557b045f9dd24f2d
Elias Feijó <eliasfeijo.dev@gmail.com>
2022-08-20 17:53:21 -0300

    cleaning project

---
+++ src/pages/index.tsx
@@ -0,0 +1,18 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>Desafio Next.js Imersão</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        <h1>Desafio Next.js Imersão</h1>
+      </main>
+    </div>
+  );
+};
+
+export default Home;
